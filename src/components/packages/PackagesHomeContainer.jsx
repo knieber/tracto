5809ba80eb2defece5508a191a62aee1d7eee252
Kyle Nieber <kyle.nieber@gmail.com>
2017-04-12 23:02:48 -0400

    add Packages components structure

---
+++ src/components/packages/PackagesHomeContainer.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class PackagesHomeContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      packages: {},
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        Hello There.
+      </div>
+    );
+  }
+}
